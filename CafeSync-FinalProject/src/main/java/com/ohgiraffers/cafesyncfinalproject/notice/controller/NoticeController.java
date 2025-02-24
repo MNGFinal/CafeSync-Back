7b18fde7df9c52e4ea2801723d7bb8418ca9e9b9
playdrew <playdrew776p@gmail.com>
2025-02-24 17:52:12 +0900

    [25-02-24] 공지사항 검색 기능

--- CafeSync-FinalProject/src/main/java/com/ohgiraffers/cafesyncfinalproject/notice/controller/NoticeController.java
+++ CafeSync-FinalProject/src/main/java/com/ohgiraffers/cafesyncfinalproject/notice/controller/NoticeController.java
@@ -9,10 +9,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -36,10 +33,19 @@ public class NoticeController {
         return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK,"공지사항 전체 조회성공",getAllNotices));
     }
 
-    @Operation(summary  = "노트 상세정보 조회", description = "노트의 상세정보 조회")
+    @Operation(summary  = "공지사항 상세정보 조회", description = "공지사항의 상세정보 조회")
     @GetMapping("/notices/{noticeCode}")
     public ResponseEntity<ResponseDTO> selectNoticeByNoteCode(@Parameter(description = "공지사항 코드", example = "1") @PathVariable int noticeCode) {
 
-        return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "노트 상세정보 조회 성공",  noticeService.selectNoticeByNoticeCode(noticeCode)));
+        return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "공지사항 상세정보 조회 성공",  noticeService.selectNoticeByNoticeCode(noticeCode)));
+    }
+
+    @Operation(summary ="공지사항 검색", description = "검색 조건에 맞는 공지사항을 조회")
+    @GetMapping("/notices/search")
+    public ResponseEntity<ResponseDTO> selectSearchProductList(
+            @RequestParam(name = "search", defaultValue = "all") String search){
+
+        return ResponseEntity
+                .ok().body(new ResponseDTO(HttpStatus.OK, "노트 검색 성공", noticeService.selectNoticeBySearch(search)));
     }
 }
