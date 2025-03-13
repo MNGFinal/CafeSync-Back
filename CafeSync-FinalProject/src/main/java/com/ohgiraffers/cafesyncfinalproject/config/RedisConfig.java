dddcf88e5c55cfb4851f987965c38748699c90d7
JungJay <keunhee0920@gmail.com>
2025-03-13 22:20:58 +0900

    [25.03.13] 배포 테스트 14번째

--- CafeSync-FinalProject/src/main/java/com/ohgiraffers/cafesyncfinalproject/config/RedisConfig.java
+++ CafeSync-FinalProject/src/main/java/com/ohgiraffers/cafesyncfinalproject/config/RedisConfig.java
@@ -12,7 +12,7 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory("localhost", 6379); // Redis 서버 주소
+        return new LettuceConnectionFactory("redis.railway.internal", 6379); // Redis 서버 주소
     }
 
     @Bean
