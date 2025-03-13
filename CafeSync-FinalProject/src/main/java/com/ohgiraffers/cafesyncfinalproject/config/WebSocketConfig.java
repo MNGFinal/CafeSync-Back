cf3125311fcfa692e33624f86d1a947a06109385
JungJay <keunhee0920@gmail.com>
2025-03-13 17:46:34 +0900

    [25.03.13] 배포 테스트

--- CafeSync-FinalProject/src/main/java/com/ohgiraffers/cafesyncfinalproject/config/WebSocketConfig.java
+++ CafeSync-FinalProject/src/main/java/com/ohgiraffers/cafesyncfinalproject/config/WebSocketConfig.java
@@ -15,7 +15,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws/chat")
-                .setAllowedOrigins("http://localhost:3000")
+                .setAllowedOrigins("cafesync-front-production.up.railway.app")
                 .withSockJS();
     }
 
